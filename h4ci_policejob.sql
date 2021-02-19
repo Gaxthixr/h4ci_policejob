a4518218cf6f37dc0167d171359c39e604269c43
H4ci <67162114+H4ci@users.noreply.github.com>
2021-02-19 20:52:57 +0100

    Update h4ci_policejob.sql

--- h4ci_policejob.sql
+++ h4ci_policejob.sql
@@ -7,9 +7,9 @@ INSERT INTO `addon_inventory` (`name`, `label`, `shared`) VALUES
 INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
 ('lspd', 'Police', 1);
 
-INSERT INTO `job_grades` (`job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
-('lspd', 0, 'cadet', 'Cadet', 100, '', ''),
-('lspd', 1, 'officier', 'Officier', 100, '', ''),
-('lspd', 2, 'sergent', 'Sergent', 100, '', ''),
-('lspd', 3, 'lieutenant', 'Lieutenant', 100, '', '')
-('lspd', 4, 'boss', 'Commandant', 100, '', '');
+INSERT INTO `job_grades` (`job_name`, `grade`, `name`, `label`, `salary`) VALUES
+('lspd', 0, 'cadet', 'Cadet', 100),
+('lspd', 1, 'officier', 'Officier', 100),
+('lspd', 2, 'sergent', 'Sergent', 100),
+('lspd', 3, 'lieutenant', 'Lieutenant', 100)
+('lspd', 4, 'boss', 'Commandant', 100);
